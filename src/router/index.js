307c376f0582306ab9ef19e11e661302b0a3c08e
Rumi <ostrich2821@gmail.com>
2025-08-30 14:40:48 +0800

    切版畫面更新

--- src/router/index.js
+++ src/router/index.js
@@ -1,21 +1,29 @@
-import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
+import { createRouter, createWebHashHistory } from 'vue-router'
+import LoginView from '../views/LoginView.vue'
+import SignUpView from '../views/SignUpView.vue'
 
 const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
+  // history: createWebHistory(import.meta.env.BASE_URL),
+  history: createWebHashHistory(import.meta.env.BASE_URL),
   routes: [
     {
       path: '/',
-      name: 'home',
-      component: HomeView,
+      name: 'login',
+      component: LoginView,
     },
     {
-      path: '/about',
-      name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (About.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import('../views/AboutView.vue'),
+      path: '/signup',
+      name: 'signup',
+      component: SignUpView,
+    },
+    {
+      path: '/todo',
+      name: 'todo',
+      component: () => import('../views/ToDoListView.vue'),
+    },
+    {
+      path: '/:pathMatch(.*)*', // catch-all route，將所有不存在的路由導回首頁
+      redirect: '/',
     },
   ],
 })
