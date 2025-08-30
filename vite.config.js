69e28877e5dc4d85fd04a1aa79a4d385fb20bc65
Rumi <ostrich2821@gmail.com>
2025-08-30 11:27:16 +0800

    init

--- vite.config.js
+++ vite.config.js
@@ -10,6 +10,7 @@ export default defineConfig({
     vue(),
     vueDevTools(),
   ],
+  base: '/2025-vue3-TodolistAPI-final/',
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
